ef5ca62c3531af4e81f7db303feb52273cc07181
syamsaiyek <msyamsundarsai@gmail.com>
2024-04-14 16:18:56 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const somedata=async()
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
